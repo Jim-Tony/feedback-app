e0f615d75362a73e7beb1c5a6102b3b276e27278
Jim-Tony <jimtonystarc@gmail.com>
2022-04-22 22:11:17 +0530

    comment added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'
+import './index.css';
+//React 18 feature
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
