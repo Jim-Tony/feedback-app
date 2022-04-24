f6ee544848ee4b85d3ba9459f6abec8da52a91d5
Jim-Tony <jimtonystarc@gmail.com>
2022-04-24 11:51:58 +0530

    added animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,10 +1,21 @@
 import Feedbackitem from "./Feedbackitem"
 import PropTypes from 'prop-types';
+import {motion, AnimatePresence} from 'framer-motion';
 function FeedbackList({feedback,onDelete}) {
   if(feedback.length===0) return <p>No Feedback lists to display</p>
   return (
     <div className="feedback-list">
-    {feedback.map(f=><Feedbackitem onDelete={onDelete} key={f.id} item={f}/>)}
+    <AnimatePresence>
+      {feedback.map(f=>
+      <motion.div
+        key={f.id}
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        exit={{opacity:0}}
+      >
+        <Feedbackitem onDelete={onDelete} key={f.id} item={f}/>
+      </motion.div>)}
+    </AnimatePresence>
     </div>
   )
 }
