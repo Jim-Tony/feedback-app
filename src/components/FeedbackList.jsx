fc84ea641e32cc56d1f6e4b1d4527f948e0c6e4c
Jim-Tony <jimtonystarc@gmail.com>
2022-04-26 21:52:40 +0530

    empty page

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,7 +4,7 @@ import FeedBackContext from './context/FeedBackContext';
 import {motion, AnimatePresence} from 'framer-motion';
 function FeedbackList() {
   const {feedBack} = useContext(FeedBackContext);
-  if(feedBack.length===0) return <p>No Feedback lists to display</p>
+  if(feedBack.length===0) return <h2 style={{margin:'20px 0px'}}>No Feedback lists to display</h2>
   return (
     <div className="feedback-list">
     <AnimatePresence>
